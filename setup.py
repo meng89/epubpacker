42c1eceaa0a8a30908ba8132c988799cd338371e
Chen Meng <observerchan@gmail.com>
2015-10-27 16:53:42 +0800

    fix: Unknown distribution option: 'install_requires'

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-# coding=utf-8
-
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
 from epubuilder import version
 
-
 setup(name='epubuilder',
       version=version,
-      description='Library to write files in the epub version 3.',
+      description='Library to write EPUB v3.',
       # long_description='',
       author='Chen Meng',
       author_email='observerchan@gmail.com',
       license='MIT',
       url='https://github.com/meng89/epubuilder',
-      py_modules=['epubuilder'],
+      # py_modules=['epubuilder'],
+      packages=['epubuilder'],
       classifiers=['Development Status :: 4 - Beta',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: MIT',
@@ -22,6 +21,5 @@ setup(name='epubuilder',
       install_requires=[
           'lxml',
           'magic',
-          'pillow',
       ]
       )
