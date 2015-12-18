ac6d858f3b95469d0ea3fde4445113488a8ef270
Observer Chan <observerchan@gmail.com>
2015-12-19 07:44:24 +0800

    none

--- epubuilder/utils.py
+++ epubuilder/utils.py
@@ -1,4 +1,4 @@
-from collections import UserList
+from collections import UserList, UserDict
 
 
 class List(UserList):
@@ -21,46 +21,68 @@ class List(UserList):
                 if self._after_add:
                     self._after_add(item=item, obj=self)
 
-    def __contains__(self, item): return item in self.data
-
-    def __len__(self): return len(self.data)
-
-    def __getitem__(self, i): return self.data[i]
-
-    def __setitem__(self, i, item):
+    def __setitem__(self, i, item):  # x[i] = item, del and add
         del self[i]
         self.insert(i, item)
 
     # all del item should be here
-    def __delitem__(self, i):
+    def __delitem__(self, i):  # del x[i], del
         if self._before_del:
             self._before_del(index=i, obj=self)
         del self.data[i]
 
-    def append(self, item):
+    def append(self, item):  # add
         self.insert(len(self), item)
 
     # all add item should be here
     def insert(self, i, item):
-        if self._before_add:
-            self._before_add(item=item, obj=self)
+        self._before_add and self._before_add(item=item, obj=self)
         self.data.insert(i, item)
 
-    def pop(self, i=-1):
+    def pop(self, i=-1):  # del
         x = self[i]
         del self[i]
         return x
 
-    def remove(self, item):
+    def remove(self, item):  # del
         i = self.index(item)
         del self[i]
 
-    def clear(self):
+    def clear(self):  # del
         for i in range(len(self)):
             self.pop()
 
-    def extend(self, other):
+    def extend(self, other):  # add
         for item in other:
             self.append(item)
 
+    def __iadd__(self, other):  # x += y, add
+        self.extend(other)
+        return self
 
+    def __imul__(self, n):  # x *= y, add
+        old_data = self.copy()
+        for x in range(n):
+            self.extend(old_data)
+        return self
+
+
+class Dict(UserDict):
+    def __init__(self, before_set=None, after_set=None, before_del=None, after_del=None):
+        super().__init__()
+        self._before_set = before_set
+        self._after_set = after_set
+        self._before_del = before_del
+        self._after_del = after_del
+
+    # all set should be here
+    def __setitem__(self, key, item):
+        self._before_set and self._before_set(key=key, item=item)
+        self.data[key] = item
+        self._after_set and self._after_set(key=key, item=item)
+
+    # all del should be here
+    def __delitem__(self, key):
+        self._before_del and self._before_del(key=key)
+        del self.data[key]
+        self._after_del and self._after_del(key=key)
