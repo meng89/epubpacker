0f99db6cc9310692cd00f052002471b0491fb5b4
Observer Chan <observerchan@gmail.com>
2016-03-05 17:52:01 +0800

    none

--- test.py
+++ test.py
@@ -1,37 +1,5 @@
 #!/usr/bin/env python3
 
-
-class D(object):
-    def __init__(self, value):
-        self.value = value
-
-<<<<<<< HEAD
-    def __get__(self, instance, owner):
-        print('get invoke')
-        return self.value
-=======
-    @property
-    def name(self):
-        raise
-        return self.__dict__['name']
->>>>>>> 0f5241ee6a9c450364f9c30a62acc9c253e685b1
-
-    def __set__(self, instance, value):
-        print('set invoke')
-        self.value = value
-
-d = D('default')
-
-<<<<<<< HEAD
-
-class X(object):
-    v = d
-=======
-class B:
-    pass
-
->>>>>>> 0f5241ee6a9c450364f9c30a62acc9c253e685b1
-
 """
 名字定死，命名空间定死。
 
@@ -41,19 +9,4 @@ class B:
 
 """
 
-<<<<<<< HEAD
-x = X()
 
-x.v = 3
-
-x.x = 4
-
-
-print(x.__dict__)
-
-
-=======
-b = B()
-
-b2 = b.__class__()
->>>>>>> 0f5241ee6a9c450364f9c30a62acc9c253e685b1
