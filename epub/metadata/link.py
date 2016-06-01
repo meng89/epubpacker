928b26ed4874a1d5956e56dff153c5e738aa96ea
Chen Meng <observerchan@gmail.com>
2016-06-01 09:44:06 +0800

    .

---
+++ epub/metadata/link.py
@@ -0,0 +1,8 @@
+class Link(_Meta):
+    @property
+    def element_name(self):
+        return 'link'
+
+    @property
+    def available_attrs(self):
+        return 'href', 'id', 'media-type', 'properties', 'rel'
