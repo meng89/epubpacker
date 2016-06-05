f9c7b71e0e029f99bc28c584b95bd3d890724599
Chen Meng <ObserverChan@gmail.com>
2016-06-05 23:13:20 +0800

    .

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ if os.path.exists('long_description.rst'):
 
 URL = 'https://github.com/meng89/{}'.format(NAME)
 
-CLASSIFIERS=['Development Status :: 4 - Beta',
-             'Intended Audience :: Developers',
-             'License :: OSI Approved :: MIT License',
-             'Programming Language :: Python :: 3',
-             'Topic :: Software Development :: Libraries :: Python Modules']
+CLASSIFIERS = ['Development Status :: 4 - Beta',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: MIT License',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development :: Libraries :: Python Modules']
 
 setup(name=NAME,
       version=VERSION,
