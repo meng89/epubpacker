99f77b0b5c567838c8266f0ea65d3edd4864e4c7
Chen Meng <ObserverChan@gmail.com>
2016-06-29 23:41:59 +0800

    .

--- epubuilder/epub2/metas.py
+++ epubuilder/epub2/metas.py
@@ -2,22 +2,55 @@ from epubuilder.xl import Element
 
 from epubuilder.public.metas.base import Base
 
+from abc import abstractmethod
 
-class Meta(Base):
-    def __init__(self, name, content):
-        super().__init__()
-        self.name = name
-        self.content = content
+
+class _Meta(Base):
+
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    @property
+    @abstractmethod
+    def content(self):
+        pass
 
     def to_element(self):
         return Element('meta', attributes={'name': self.name, 'content': self.content})
 
 
-class Cover(Base):
+class Meta(_Meta):
+    def __init__(self, name, content):
+        super().__init__()
+        self._name = name
+        self._content = content
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def content(self):
+        return self._content
+
+    def to_element(self):
+        return Element('meta', attributes={'name': self.name, 'content': self.content})
+
+
+class Cover(_Meta):
     def __init__(self, file):
         super().__init__()
         self._file = file
 
-    def to_element(self):
-        return Meta(name='cover', content=self._file.identification).to_element()
+    @property
+    def name(self):
+        return 'cover'
 
+    @property
+    def content(self):
+        return self._file.identification
+
+    def to_element(self):
+        return Element('meta', attributes={'name': self.name, 'content': self._file.identification})
