d41dea9fa93218d8883ea9240fa505219322be5e
Chen Meng <ObserverChan@gmail.com>
2016-07-10 20:50:55 +0800

    .

--- README.rst
+++ README.rst
@@ -1,6 +1,7 @@
 Epubuilder
 ==========
 
-Epubuilder is a Python 3 module to build EPUB 2 or 3 document from web files and related information.
+Epubuilder is a Python module to build EPUB 2 or 3 document from web files and related information.
+
 
 For more information visit http://epubuilder.readthedocs.io
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 from distutils.util import convert_path
-import os 
+
 
 NAME = "epubuilder"
 
@@ -9,28 +9,30 @@ ver_path = convert_path('{}/version.py'.format(NAME))
 with open(ver_path) as ver_file:
     exec(ver_file.read(), main_ns)
 
-# DESCRIPTION = 'A library to write EPUB v3.'
-
-LONG_DESCRIPTION = ''
-if os.path.exists('long_description.rst'):
-    LONG_DESCRIPTION = open('long_description.rst').read()
+DESCRIPTION = 'A module to build EPUB 2 or 3 document.'
 
 
 URL = 'https://github.com/meng89/{}' + NAME
 
-CLASSIFIERS = ['Development Status :: 4 - Beta',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved :: MIT License',
-               'Programming Language :: Python :: 3',
-               'Topic :: Software Development :: Libraries :: Python Modules']
-
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name=NAME,
       version=main_ns['__version__'],
-      # description=DESCRIPTION,
-      # long_description=LONG_DESCRIPTION,
       include_package_data=True,
       author='Chen Meng',
       author_email='ObserverChan@gmail.com',
