b31f8f8d846e3cad12c033843d79e11833fbae95
Chen Meng <ObserverChan@gmail.com>
2016-07-11 23:21:01 +0800

    more test action

--- test/test_xl/__init__.py
+++ test/test_xl/__init__.py
@@ -1,24 +1,36 @@
 # coding=utf-8
 
+from xml.etree import ElementTree as Et
+
 from epubuilder.xl import parse, clean_whitespaces, pretty_insert
 
 import os
 
 path = os.path.dirname(__file__)
 
+TEST_XML_PATH = os.path.join(path, 'test.xml')
+
 
 def test_parse():
-    # return
-    xmlstr = open(os.path.join(path, 'et.xml')).read()
-    print(type(xmlstr))
-    x = parse(xmlstr)
+    xmlstr = open(TEST_XML_PATH).read()
+    xl = parse(xmlstr)
 
-    e = x.root
-    e = clean_whitespaces(e)
-    e = pretty_insert(e, dont_do_when_one_child=True)
+    e = xl.root
 
-    # print(e.string())
+    clean_e = clean_whitespaces(e)
 
+    pretty_e = pretty_insert(clean_e, dont_do_when_one_child=True)
 
-def test_element():
-    pass
+    assert clean_e.string() == clean_whitespaces(pretty_e).string()
+
+    assert pretty_e.string() == pretty_insert(clean_e, dont_do_when_one_child=True).string()
+
+    assert clean_e.string() != pretty_e.string()
+
+    Et.fromstring(xl.string())
+
+    xl.root = clean_e
+    Et.fromstring(xl.string())
+
+    xl.root = pretty_e
+    Et.fromstring(xl.string())
